352acad34345f8a6ac9016e37751ae36a1bb8dde
Amita <naikadarsh654@gmail.com>
2026-09-25 19:39:20 +0530

    Fix next config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/EventStudio',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
